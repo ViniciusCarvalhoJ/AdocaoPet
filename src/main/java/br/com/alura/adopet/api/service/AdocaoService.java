6f94333a0b6400aedc18213e2177e49ef6407be9
Vinicius Carvalho <viniciuscarvalho.dev12@gmail.com>
2025-01-22 15:53:12 -0300

    Subindo as primeiras alteracoes no projeto

---
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -0,0 +1,78 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+public class AdocaoService {
+
+    @Autowired
+    private AdocaoRepository repository;
+    @Autowired
+    private EmailService emailService;
+
+    public ResponseEntity<String> solicitar(Adocao adocao) {
+        if (adocao.getPet().getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+
+        } else {
+            List<Adocao> adocoes = repository.findAll();
+            for (Adocao a : adocoes) {
+                if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+
+                    return ResponseEntity.badRequest().body("Tutor já possui outra adoção aguardando avaliação!");
+                }
+            }
+            for (Adocao a : adocoes) {
+                if (a.getPet() == adocao.getPet() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+                    return ResponseEntity.badRequest().body("Pet já está aguardando avaliação para ser adotado!");
+                }
+            }
+            for (Adocao a : adocoes) {
+                int contador = 0;
+                if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.APROVADO) {
+                    contador = contador + 1;
+                }
+                if (contador == 5) {
+                    return ResponseEntity.badRequest().body("Tutor chegou ao limite máximo de 5 adoções!");
+                }
+            }
+        }
+        adocao.setData(LocalDateTime.now());
+        adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
+        repository.save(adocao);
+        ("Parabéns " + adocao.getTutor().getNome() +
+                "!\n\nSua adoção do pet " + adocao.getPet().getNome() +
+                ", solicitada em " + adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +
+                ", foi aprovada.\nFavor entrar em contato com o abrigo " + adocao.getPet().getAbrigo().getNome() +
+                " para agendar a busca do seu pet."
+        emailService.encaminharEmail(adocao.getPet().getAbrigo().getEmail(), "", );
+        return null;
+    }
+
+    public void aprovar(Adocao adocao) {
+        adocao.setStatus(StatusAdocao.APROVADO);
+        repository.save(adocao);
+        emailService.encaminharEmail(adocao);
+
+    }
+
+    public void reprovar(Adocao adocao) {
+        adocao.setStatus(StatusAdocao.REPROVADO);
+        emailService.encaminharEmail(adocao);
+        repository.save(adocao);
+
+
+    }
+}
